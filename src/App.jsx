5c94d9f609179933097dc5c0de0c49de19bd5342
shaharman18 <shahaman604@gmail.com>
2026-02-13 03:19:58 +0530

    node mail errror

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Hero, About, Skills, Projects, TechArchitecture, Contact, Footer, CustomCursor, AdminDashboard } from './components';
 import { useEffect } from 'react';
 
+import api from './api/axios';
+
 function App() {
   useEffect(() => {
     window.scrollTo(0, 0);
+
+    // Wake up the Render server immediately on page load
+    api.get('/health').catch(() => console.log('Server warming up...'));
 
     if (window.location.hash) {
       window.history.replaceState(null, null, ' ');
